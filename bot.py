094c3ecf4622c44786ae1f6c40e10740e0e8a7ea
Lyra <github@sblume.eu>
2018-06-04 07:50:30 +0200

    Allow to select plants with an inline keyboard

--- bot.py
+++ bot.py
@@ -1,16 +1,30 @@
 import logging
 import re
+from uuid import uuid4
 
-from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, InlineQueryHandler
 
 import config
 from api.plant import get_humidity
 
 
 def plant(bot, update):
-    plant_number = re.sub('^/plant','', update.message.text)
-    humidity =  get_humidity(plant_number)
-    update.message.reply_text('The plants humidity is {0:.0f}%'.format(humidity))
+    plant_number = re.sub('^/plant ', '', update.message.text)
+    if plant_number.isdigit():
+        humidity = get_humidity(plant_number)
+        update.message.reply_text('The plants humidity is {0:.0f}%'.format(humidity))
+    else:
+        keyboard = [[InlineKeyboardButton("Minze", callback_data='10')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+
+def selected_plant(bot, update):
+    query = update.callback_query
+    bot.edit_message_text(text='The plants humidity is {0:.0f}%'.format(get_humidity(query.data)),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
 
 
 logging.basicConfig(
@@ -18,5 +32,6 @@ logging.basicConfig(
     level=logging.INFO)
 updater = Updater(config.telegram_api)
 updater.dispatcher.add_handler(CommandHandler('plant', plant))
+updater.dispatcher.add_handler(CallbackQueryHandler(selected_plant))
 updater.start_polling()
 updater.idle()
